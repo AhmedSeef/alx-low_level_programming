8dfb07097cd4fd3f324dad295feea914ef5a8eeb
AhmedSeef <ahmed.seef.gooda@gmail.com>
2023-09-07 16:15:53 -0700

    add 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- *  main - prints with proper grammar, but the outcome is a piece of art,Add new line with \n
+ *  main - prints with proper grammar, but the outcome is a piece of art
+ *  Add new line with \n
  *  Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
