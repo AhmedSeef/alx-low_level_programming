7a6672f54e9792ccc06f709faaeaa2fced294b1d
AhmedSeef <ahmed.seef.gooda@gmail.com>
2023-09-08 14:20:48 -0700

    add 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - from 0 to 9 using int
+ * Return : Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
